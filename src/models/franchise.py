591c9dee1b6f446033fb65cbc88948c325255e11
moospfote <vincent12xx@protonmail.com>
2026-02-14 12:40:36 +0100

    Add Franchise data model with initialization parameters

---
+++ src/models/franchise.py
@@ -0,0 +1,6 @@
+class Franchise:
+    """Data model for a franchise"""
+    def __init__(self, franchise_id=None, franchise_name='', franchise_info=''):
+        self.franchise_id = franchise_id
+        self.franchise_name = franchise_name
+        self.franchise_info = franchise_info
